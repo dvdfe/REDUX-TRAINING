2fad2ca4d7a7178a75375b57dd5343866333df14
DVDFE <davidfe.pro@gmail.com>
2023-08-02 18:53:15 +0200

    ajout post reducer et application de celui-ci dans une requete get

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import "./styles/index.scss";
 //Config REDUX
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers"
-
-
+import rootReducer from "./reducers";
+import { getPosts } from "./actions/post.action";
 
 const store = configureStore({
-    reducer: rootReducer,
-    devTools: true,
-})
+  reducer: rootReducer,
+  devTools: true,
+});
 
+store.dispatch(getPosts())
 
 ReactDOM.render(
-    <Provider store={store}>
+  <Provider store={store}>
+    <App />
+  </Provider>,
 
-<App />
-    </Provider>
-
-, document.getElementById("root"));
+  document.getElementById("root")
+);
