905f0fd073c9f70cd906d10e173314be54c6d993
DVDFE <davidfe.pro@gmail.com>
2023-08-07 13:02:23 +0200

    ajout de données de manière dynamique dans le component user.js grace à userselector

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import userReducer from '../reducers/user.reducer';
 
 const User = () => {
+
+const user = useSelector((state) => state.userReducer);
+console.log(user)
+
   return (
     <div className="user-container">
       <div className="user">
-        <h3>pseudo</h3>
+        <h3>{user.pseudo}</h3>
         <img src="./img/bill-gates.png" alt="bill gates" />
-        <p>Age : 30 ans</p>
-        <p>Like(s) : 0</p>
+        <p>Age : {user.age} ans</p>
+        <p>Like(s) : {user.likes}</p>
       </div>
     </div>
   );
